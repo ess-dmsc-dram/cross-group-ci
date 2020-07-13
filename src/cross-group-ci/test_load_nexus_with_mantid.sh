78c5fad4faba7f40b844bfbf065e17edfa1867da
Neil Vaytet <neil.vaytet@esss.se>
2020-07-13 16:05:34 +0200

    print messages

--- src/cross-group-ci/test_load_nexus_with_mantid.sh
+++ src/cross-group-ci/test_load_nexus_with_mantid.sh
@@ -4,13 +4,18 @@
 
 cd $HOME;
 cd generate-nexus-files;
+echo "Updating generate-nexus-files";
 git pull;
 cd src/nexusjson;
+echo "Updating nexusjson";
 git pull;
 cd ../nexusutils;
+echo "Updating nexusutils"
 git pull;
 
 cd ${HOME}/mantid/source;
+echo "Updating mantid";
 git pull
 cd ../build
+echo "Building mantid";
 make -j 4 install;
