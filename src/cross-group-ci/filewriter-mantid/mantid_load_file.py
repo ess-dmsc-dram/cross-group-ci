fdd947948c115a6a371592e0e38d28b33c7c86a4
Neil Vaytet <neil.vaytet@esss.se>
2020-07-20 22:15:44 +0200

    fix filename arg

--- src/cross-group-ci/filewriter-mantid/mantid_load_file.py
+++ src/cross-group-ci/filewriter-mantid/mantid_load_file.py
@@ -2,9 +2,8 @@ import argparse
 import mantid.simpleapi as mantid
 
 parser = argparse.ArgumentParser(description='Load a Nexus file using Mantid.')
-parser.add_argument('filename', type=str, nargs=1,
-                    help='a file name to be loaded')
+parser.add_argument('filename', type=str, help='a file name to be loaded')
 args = parser.parse_args()
-print(args.filename)
+
 ws = mantid.Load(Filename=args.filename)
 print(ws)
