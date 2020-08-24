3b76d27aab2c1281f742108ae71182564075f4fd
Neil Vaytet <neil.vaytet@esss.se>
2020-08-24 10:27:14 +0200

    cannot check tof values as they are randomly generated. Only check the number of events

--- src/cross-group-ci/filewriter-mantid/test_load_nexus_files.py
+++ src/cross-group-ci/filewriter-mantid/test_load_nexus_files.py
@@ -5,6 +5,3 @@ import pytest
 def test_load_wish_file():
     ws = load(filename='WISH_example.nxs')
     assert ws.getNumberEvents() == 1000
-    assert ws.getTofMin() == pytest.approx(0.439)
-    assert ws.getTofMax() == pytest.approx(49983.7)
-
