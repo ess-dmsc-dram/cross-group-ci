c957a0f9cd1b3235281c5ecae0b2aa4272bd602a
Neil Vaytet <neil.vaytet@esss.se>
2020-08-24 11:44:12 +0200

    make new log file from date

--- src/cross-group-ci/run_cross_group_testing.sh
+++ src/cross-group-ci/run_cross_group_testing.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# Generate new log file name
+today=$(date "+%Y-%m-%d");
+logfile=${HOME}/logs/${today}.log;
+
 # Get list of all dirs
 dirlist=$(ls -d */);
 
 # Iterate over the list and run the tests inside each dir
 for test_dir in ${dirlist} ; do
     cd ${test_dir};
-    bash setup_and_run.sh;
+    bash setup_and_run.sh > ${logfile} 2>&1;
     cd ../;
 done
