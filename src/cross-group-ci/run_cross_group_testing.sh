510a1d071d814df436d3daf79d864b36c6baf15d
Neil Vaytet <neil.vaytet@esss.se>
2020-08-24 12:00:02 +0200

    add path to driver script

--- src/cross-group-ci/run_cross_group_testing.sh
+++ src/cross-group-ci/run_cross_group_testing.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+divider="========================"
+
 # Generate new log file name
 today=$(date "+%Y-%m-%d");
 logfile=${HOME}/logs/${today}.log;
 touch ${logfile};
+
+# Get directory where this script is located and cd into it
+CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )";
+cd ${CWD};
 
 # Get list of all dirs
 dirlist=$(ls -d */);
@@ -11,7 +17,9 @@ dirlist=$(ls -d */);
 # Iterate over the list and run the tests inside each dir
 for test_dir in ${dirlist} ; do
     cd ${test_dir};
+    echo ${divider} >> ${logfile};
     echo ${test_dir} >> ${logfile};
     bash setup_and_run.sh >> ${logfile} 2>&1;
+    echo ${divider} >> ${logfile};
     cd ../;
 done
